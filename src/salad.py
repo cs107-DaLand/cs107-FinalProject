dc050420bec95acbfbdaefe5d471c9fb54435f4d
sehgal-neil <35875729+neil-sehgal@users.noreply.github.com>
2021-11-04 17:01:53 -0400

    Added mul and rmul, added support for multi-dimensional input in constructor

--- src/salad.py
+++ src/salad.py
@@ -4,6 +4,9 @@ from utils import add_dict
 class Variable(object):
 	counter = 0
 	def __init__(self, val, der=None, label=None, ad_mode='forward'):
+		
+		self.val = np.asarray(val)
+		self.ad_mode = ad_mode
 
 		if label is not None:
 			self.label = label
@@ -13,10 +16,7 @@ class Variable(object):
 		if der is not None:
 			self.der = der
 		else:
-			self.der = {self.label: np.asarray([[1]])}
-
-		self.ad_mode = ad_mode
-		self.val = val
+			self.der = {self.label: np.ones(self.val.shape)}
 
 		Variable.increment()
 
@@ -34,8 +34,6 @@ class Variable(object):
 
 
 	def __add__(self, other):
-		# print(self)
-		# print(other)
 		try:
 			return Variable(self.val + other.val, add_dict(self.der, other.der))
 		except AttributeError:
@@ -46,18 +44,32 @@ class Variable(object):
 		return self.__add__(other)
 
 	def __sub__(self, other):
-		pass
-		return self.__add__(-other)
+		return self.__add__(-1*other)
 
 	def __rsub__(self, other):
-		pass
-		return (-self).__add__(other)
+		return (-1*self).__add__(other)
 
 	def __mul__(self, other):
-		pass
+		try:
+			a1, a2 = self.val, other.val
+			dict1, dict2 = self.der, other.der
+			new_dict = {}
+			for v in set(dict1).intersection(set(dict2)): #intersection
+				new_dict[v] = a1*dict2[v] + a2*dict1[v]
+			for v in set(dict1) - set(dict2): # Only those variables in self
+				new_dict[v] = a2*dict1[v]
+			for v in set(dict2) - set(dict1): # Only those variables in other
+				new_dict[v] = a1*dict2[v]
+			return Variable(a1*a2, new_dict)
+
+		except AttributeError:
+			new_dict = {}
+			for key in self.der:		
+				new_dict[key] = other*self.der[key]	
+			return Variable(self.val * other, new_dict)
 
 	def __rmul__(self, other):
-		pass
+		return self.__mul__(other)
 
 	def __truediv__(self, other):
 		pass
