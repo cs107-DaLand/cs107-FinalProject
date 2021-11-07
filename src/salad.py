ea256e656df150c22b2619ab1f70d9d6a4d5746c
Lotus Xia <lingjun.lxia@gmail.com>
2021-11-07 14:09:58 -0500

    Add exp, log10, ln, logistic code

--- src/salad.py
+++ src/salad.py
@@ -1,102 +1,181 @@
 import numpy as np
 from utils import add_dict
+import copy
+
 
 class Variable(object):
-	counter = 0
-	def __init__(self, val, der=None, label=None, ad_mode='forward', increment_counter=True):
-		
-		self.val = np.asarray(val)
-		self.ad_mode = ad_mode
+    counter = 0
 
-		if label is not None:
-			self.label = label
-		else:
-			self.label = 'v' + Variable.get_counter()
+    def __init__(
+        self, val, der=None, label=None, ad_mode="forward", increment_counter=True
+    ):
 
-		if der is not None:
-			self.der = der
-		else:
-			self.der = {self.label: np.ones(self.val.shape)}
+        self.val = np.asarray(val)
+        self.ad_mode = ad_mode
 
-		if increment_counter:
-			Variable.increment()
+        if label is not None:
+            self.label = label
+        else:
+            self.label = "v" + Variable.get_counter()
+
+        if der is not None:
+            self.der = der
+        else:
+            self.der = {self.label: np.ones(self.val.shape)}
+
+        if increment_counter:
+            Variable.increment()
+
+    def __str__(self):
+        return f"Label: {self.label}, Value: {self.val}, Derivative: {self.der}"
+
+    @staticmethod
+    def increment():
+        Variable.counter += 1
+
+    @staticmethod
+    def get_counter():
+        return str(Variable.counter)
+
+    def __add__(self, other):
+        try:
+            return Variable(self.val + other.val, add_dict(self.der, other.der))
+        except AttributeError:
+            return Variable(self.val + other, self.der)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        return self.__add__(-1 * other)
+
+    def __rsub__(self, other):
+        return (-1 * self).__add__(other)
+
+    def __mul__(self, other):
+        try:
+            a1, a2 = self.val, other.val
+            dict1, dict2 = self.der, other.der
+            new_dict = {}
+            for v in set(dict1).intersection(set(dict2)):  # intersection
+                new_dict[v] = a1 * dict2[v] + a2 * dict1[v]
+            for v in set(dict1) - set(
+                dict2
+            ):  # Only those variables in self DOES THIS WORK
+                new_dict[v] = a2 * dict1[v]
+            for v in set(dict2) - set(dict1):  # Only those variables in other
+                new_dict[v] = a1 * dict2[v]
+            return Variable(a1 * a2, new_dict)
+
+        except AttributeError:
+            new_dict = {}
+            for key in self.der:
+                new_dict[key] = other * self.der[key]
+            return Variable(self.val * other, new_dict)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        try:
+            # divide other by 1
+            a2 = 1 / other.val
+            dict2 = dict(other.der)
+            for k in dict2:
+                dict2[k] = (-1 * dict2[k]) / (other.val * other.val)
+            reciprocal_other = Variable(a2, dict2, increment_counter=False)
+            return self.__mul__(reciprocal_other)
+
+        except AttributeError:
+            # divide self by other
+            a1 = self.val / other
+            dict1 = dict(self.der)
+            for k in dict1:
+                dict1[k] = (other * dict1[k]) / (other * other)
+            return Variable(a1, dict1)
+
+    def __rtruediv__(self, other):
+        new_var = self.__truediv__(other)
+        # return reciprocal
+        a2 = 1 / new_var.val
+        dict2 = dict(new_var.der)
+        for k in dict2:
+            dict2[k] = (-1 * dict2[k]) / (new_var.val * new_var.val)
+        reciprocal_new_var = Variable(a2, dict2, increment_counter=False)
+        return reciprocal_new_var
 
 
-	def __str__(self):
-		return f"Label: {self.label}, Value: {self.val}, Derivative: {self.der}"
+def exp(x):
+    """
+    If x is a Variable, returns a new variable with val and der
+    If x is a number, returns numeric exp(x)
+    """
 
-	@staticmethod
-	def increment():
-		Variable.counter += 1
+    if isinstance(x, Variable):
+        val = np.exp(x.val)
+        der = copy.deepcopy(x.der)
+        for key in der:
+            der[key] *= val * x.der[key]
+        return Variable(val, der, increment_counter=True)
 
-	@staticmethod
-	def get_counter():
-		return str(Variable.counter)
-
-
-	def __add__(self, other):
-		try:
-			return Variable(self.val + other.val, add_dict(self.der, other.der))
-		except AttributeError:
-			return Variable(self.val + other, self.der)
-
-
-	def __radd__(self, other):
-		return self.__add__(other)
-
-	def __sub__(self, other):
-		return self.__add__(-1*other)
-
-	def __rsub__(self, other):
-		return (-1*self).__add__(other)
-
-	def __mul__(self, other):
-		try:
-			a1, a2 = self.val, other.val
-			dict1, dict2 = self.der, other.der
-			new_dict = {}
-			for v in set(dict1).intersection(set(dict2)): #intersection
-				new_dict[v] = a1*dict2[v] + a2*dict1[v]
-			for v in set(dict1) - set(dict2): # Only those variables in self DOES THIS WORK
-				new_dict[v] = a2*dict1[v]
-			for v in set(dict2) - set(dict1): # Only those variables in other
-				new_dict[v] = a1*dict2[v]
-			return Variable(a1*a2, new_dict)
-
-		except AttributeError:
-			new_dict = {}
-			for key in self.der:		
-				new_dict[key] = other*self.der[key]	
-			return Variable(self.val * other, new_dict)
-
-	def __rmul__(self, other):
-		return self.__mul__(other)
-
-	def __truediv__(self, other):
-		try:
-			# divide other by 1
-			a2 = 1/other.val
-			dict2 = dict(other.der)
-			for k in dict2:
-				dict2[k] = (-1*dict2[k]) / (other.val * other.val)
-			reciprocal_other = Variable(a2, dict2, increment_counter=False)
-			return self.__mul__(reciprocal_other)
-
-		except AttributeError:
-			# divide self by other
-			a1 = self.val/other
-			dict1 = dict(self.der)
-			for k in dict1:
-				dict1[k] = (other*dict1[k]) / (other*other)
-			return Variable(a1, dict1)
+    elif isinstance(x, (int, float)):
+        return np.exp(x)
+    else:
+        raise TypeError("x must be a Variable or a number")
 
 
-	def __rtruediv__(self, other):
-		new_var = self.__truediv__(other)
-		# return reciprocal
-		a2 = 1/new_var.val
-		dict2 = dict(new_var.der)
-		for k in dict2:
-			dict2[k] = (-1*dict2[k]) / (new_var.val * new_var.val)
-		reciprocal_new_var = Variable(a2, dict2, increment_counter=False)
-		return reciprocal_new_var
+def log10(x):
+    """
+    If x is a Variable, returns a new variable with val and der
+    If x is a number, returns numeric log(x)
+    """
+    if isinstance(x, Variable):
+        val = np.log(x.val) / np.log(10)
+        der = copy.deepcopy(x.der)
+        for key in der:
+            der[key] *= 1 / x.val * 1 / np.log(10) * x.der[key]
+        return Variable(val, der, increment_counter=True)
+
+    elif isinstance(x, (int, float)):
+        return np.log(x) / np.log(10)
+    else:
+        raise TypeError("x must be a Variable or a number")
+
+    # 1/x * 1/log(10)
+
+
+def ln(x):
+    """
+    If x is a Variable, returns a new variable with val and der
+    If x is a number, returns numeric ln(x)
+    """
+    if isinstance(x, Variable):
+        val = np.log(x.val)
+        der = copy.deepcopy(x.der)
+        for key in der:
+            der[key] *= 1 / x.val * x.der[key]
+        return Variable(val, der, increment_counter=True)
+
+    elif isinstance(x, (int, float)):
+        return np.log(x)
+    else:
+        raise TypeError("x must be a Variable or a number")
+
+
+def logistic(x):
+    """
+    If x is a Variable, returns a new variable with val and der
+    If x is a number, returns numeric 1 / (1 + np.exp(x))
+    """
+    if isinstance(x, Variable):
+        val = 1 / (1 + np.exp(-x.val))
+        der = copy.deepcopy(x.der)
+        for key in der:
+            # derivative of logistic function is logistic(x) * (1 - logistic(x))
+            der[key] *= val * (1 - val) * x.der[key]
+        return Variable(val, der, increment_counter=True)
+
+    elif isinstance(x, (int, float)):
+        return 1 / (1 + np.exp(-x))
+    else:
+        raise TypeError("x must be a Variable or a number")
--- src/test_forward.py
+++ src/test_forward.py
@@ -1,9 +1,12 @@
 import pytest
+import math
 import numpy as np
-from salad import *
+import autograd.numpy as adnp
+from autograd import grad
+import salad as ad
 
 ## For comparing derivative dictionaries with rounding
-def compare_dicts(dict1, dict2, round_place = 4):
+def compare_dicts(dict1, dict2, round_place=4):
     for k in dict2:
         if np.round(dict1[k], round_place) != np.round(dict2[k], round_place):
             return False
@@ -11,146 +14,319 @@ def compare_dicts(dict1, dict2, round_place = 4):
 
 
 def test_add_radd():
-    x = Variable(3)
+    x = ad.Variable(3)
     y = x + 3
 
     assert y.val == 6
     assert list(y.der.values()) == np.array([1])
 
-    x = Variable(3)
+    x = ad.Variable(3)
     y = 3 + x
 
     assert y.val == 6
     assert list(y.der.values()) == np.array([1])
 
-    x = Variable(3, {'x': 1})
-    y = Variable(3, {'y': 1})
+    x = ad.Variable(3, {"x": 1})
+    y = ad.Variable(3, {"y": 1})
     z = x + y
     assert z.val == 6
-    assert z.der == {'x': 1, 'y': 1}
+    assert z.der == {"x": 1, "y": 1}
 
-
-    x = Variable(np.ones((5,5)), label='x')
-    y = Variable(np.ones((5,5)), label='y')
+    x = ad.Variable(np.ones((5, 5)), label="x")
+    y = ad.Variable(np.ones((5, 5)), label="y")
     z = x + y
-    assert np.array_equal(z.val, 2*np.ones((5,5)))
-    np.testing.assert_equal(z.der, {'x': np.ones((5,5)), 'y': np.ones((5,5))})
+    assert np.array_equal(z.val, 2 * np.ones((5, 5)))
+    np.testing.assert_equal(z.der, {"x": np.ones((5, 5)), "y": np.ones((5, 5))})
 
-    z = x+x+y+y+2
-    assert np.array_equal(z.val, 4*np.ones((5,5))+2)
-    np.testing.assert_equal(z.der, {'x': 2*np.ones((5,5)), 'y': 2*np.ones((5,5))})
-
+    z = x + x + y + y + 2
+    assert np.array_equal(z.val, 4 * np.ones((5, 5)) + 2)
+    np.testing.assert_equal(z.der, {"x": 2 * np.ones((5, 5)), "y": 2 * np.ones((5, 5))})
 
 
 def test_sub_rsub():
-    x = Variable(3)
+    x = ad.Variable(3)
     y = x - 3
 
     assert y.val == 0
     assert list(y.der.values()) == np.array([1])
 
-    x = Variable(3)
+    x = ad.Variable(3)
     y = 3 - x
 
     assert y.val == 0
     assert list(y.der.values()) == np.array([-1])
 
-    x = Variable(3, {'x': 1})
-    y = Variable(3, {'y': 1})
+    x = ad.Variable(3, {"x": 1})
+    y = ad.Variable(3, {"y": 1})
     z = x - y
     assert z.val == 0
-    assert z.der == {'x': 1, 'y': -1}
+    assert z.der == {"x": 1, "y": -1}
 
-
-    x = Variable(np.ones((5,5)), label='x')
-    y = Variable(np.ones((5,5)), label='y')
+    x = ad.Variable(np.ones((5, 5)), label="x")
+    y = ad.Variable(np.ones((5, 5)), label="y")
     z = x - y
-    assert np.array_equal(z.val, np.zeros((5,5)))
-    np.testing.assert_equal(z.der, {'x': np.ones((5,5)), 'y': -1*np.ones((5,5))})
+    assert np.array_equal(z.val, np.zeros((5, 5)))
+    np.testing.assert_equal(z.der, {"x": np.ones((5, 5)), "y": -1 * np.ones((5, 5))})
 
-    z = x+x-y-y+2
-    assert np.array_equal(z.val, 2*np.ones((5,5)))
-    np.testing.assert_equal(z.der, {'x': 2*np.ones((5,5)), 'y': -2*np.ones((5,5))})
+    z = x + x - y - y + 2
+    assert np.array_equal(z.val, 2 * np.ones((5, 5)))
+    np.testing.assert_equal(
+        z.der, {"x": 2 * np.ones((5, 5)), "y": -2 * np.ones((5, 5))}
+    )
+
 
 def test_mul_rmul():
-    x = Variable(3, label='x')
+    x = ad.Variable(3, label="x")
     y = x * 2
     assert y.val == 6
-    assert y.der == {'x':  2}
+    assert y.der == {"x": 2}
 
-    #y = 5x + x^2
-    y = x*2 + 3*x + x * x 
+    # y = 5x + x^2
+    y = x * 2 + 3 * x + x * x
     assert y.val == 24
-    assert y.der == {'x': 11}
+    assert y.der == {"x": 11}
 
-    x = Variable(3, label='x')
-    y = Variable(2, label='y')
-    z = x*y
-    assert z.val == 6
-    assert z.der == {'x': 2, 'y': 3}
-
-    z = 3*z*3
-    assert z.val == 54
-    assert z.der == {'x': 18, 'y': 27}
-
-    x = Variable(3, label='x')
-    y = Variable(2, label='y')
-    z = x*y
-    z = y*z # y^2*x
-    assert z.val == 12
-    assert z.der == {'x': y.val**2, 'y': 2*y.val*x.val}
-
-
-    x = Variable(2*np.ones((5,5)), label='x')
-    y = Variable(3*np.ones((5,5)), label='y')
+    x = ad.Variable(3, label="x")
+    y = ad.Variable(2, label="y")
     z = x * y
-    assert np.array_equal(z.val, 2*3*np.ones((5,5)))
-    np.testing.assert_equal(z.der, {'x': 3*np.ones((5,5)), 'y': 2*np.ones((5,5))})
+    assert z.val == 6
+    assert z.der == {"x": 2, "y": 3}
 
-    z = -1*z * x # f = -(x^2) * y, dx = -2xy, dy = -x^2
-    assert np.array_equal(z.val, -12*np.ones((5,5)))
-    np.testing.assert_equal(z.der, {'x': -2*2*3*np.ones((5,5)), 'y': -1*2*2*np.ones((5,5))})
+    z = 3 * z * 3
+    assert z.val == 54
+    assert z.der == {"x": 18, "y": 27}
+
+    x = ad.Variable(3, label="x")
+    y = ad.Variable(2, label="y")
+    z = x * y
+    z = y * z  # y^2*x
+    assert z.val == 12
+    assert z.der == {"x": y.val ** 2, "y": 2 * y.val * x.val}
+
+    x = ad.Variable(2 * np.ones((5, 5)), label="x")
+    y = ad.Variable(3 * np.ones((5, 5)), label="y")
+    z = x * y
+    assert np.array_equal(z.val, 2 * 3 * np.ones((5, 5)))
+    np.testing.assert_equal(z.der, {"x": 3 * np.ones((5, 5)), "y": 2 * np.ones((5, 5))})
+
+    z = -1 * z * x  # f = -(x^2) * y, dx = -2xy, dy = -x^2
+    assert np.array_equal(z.val, -12 * np.ones((5, 5)))
+    np.testing.assert_equal(
+        z.der, {"x": -2 * 2 * 3 * np.ones((5, 5)), "y": -1 * 2 * 2 * np.ones((5, 5))}
+    )
 
 
 def test_truediv_rtruediv():
-    x = Variable(3, label='x')
+    x = ad.Variable(3, label="x")
     y = x / 2
     assert y.val == 1.5
-    assert y.der == {'x':  1/2}
+    assert y.der == {"x": 1 / 2}
 
-    
-    y = x/2 + 3/x + x / x 
+    y = x / 2 + 3 / x + x / x
     assert y.val == 3.5
-    assert y.der == {'x': .5 - 3/9}
+    assert y.der == {"x": 0.5 - 3 / 9}
 
-    x = Variable(3, label='x')
-    y = Variable(2, label='y')
-    z = x/y
-    assert z.val == 3/2
-    assert z.der == {'x': 1/2, 'y': -3/4} # dx = 1/y, dy = -x/y^2
-
-    z = 2.4/z/x/8 # 2.4/(x/y)/x/8
-    assert z.val == 2.4/(3/2)/3/8
-    ## Using this function because of rounding errors
-    assert compare_dicts(z.der, {'x': (-.6*y.val)/(x.val**3), 'y': (.3/(x.val**2))}) #dx = -.6y/x^3 , dy = .3/x^2
-    
-
-    x = Variable(2*np.ones((5,5)), label='x')
-    y = Variable(3*np.ones((5,5)), label='y')
+    x = ad.Variable(3, label="x")
+    y = ad.Variable(2, label="y")
     z = x / y
-    assert np.array_equal(z.val, 2/3*np.ones((5,5)))
-    np.testing.assert_equal(z.der, {'x': 1/y.val, 'y': -1*x.val/(y.val**2)})
+    assert z.val == 3 / 2
+    assert z.der == {"x": 1 / 2, "y": -3 / 4}  # dx = 1/y, dy = -x/y^2
 
-    z = -1/ z / x 
-    assert np.array_equal(z.val, -1 / (2/3) / 2 *np.ones((5,5)))
-    np.testing.assert_equal(z.der, {'x': 2 * y.val / (x.val**3), 'y': -1 / (x.val**2)})
-    
+    z = 2.4 / z / x / 8  # 2.4/(x/y)/x/8
+    assert z.val == 2.4 / (3 / 2) / 3 / 8
+    ## Using this function because of rounding errors
+    assert compare_dicts(
+        z.der, {"x": (-0.6 * y.val) / (x.val ** 3), "y": (0.3 / (x.val ** 2))}
+    )  # dx = -.6y/x^3 , dy = .3/x^2
+
+    x = ad.Variable(2 * np.ones((5, 5)), label="x")
+    y = ad.Variable(3 * np.ones((5, 5)), label="y")
+    z = x / y
+    assert np.array_equal(z.val, 2 / 3 * np.ones((5, 5)))
+    np.testing.assert_equal(z.der, {"x": 1 / y.val, "y": -1 * x.val / (y.val ** 2)})
+
+    z = -1 / z / x
+    assert np.array_equal(z.val, -1 / (2 / 3) / 2 * np.ones((5, 5)))
+    np.testing.assert_equal(
+        z.der, {"x": 2 * y.val / (x.val ** 3), "y": -1 / (x.val ** 2)}
+    )
 
 
-test_add_radd()
+def test_exp():
+    print("-" * 50, "Test exp")
+    x = 3
+    ans = ad.exp(x)
+    sol = np.exp(x)
+    print("ad.exp(3)")
+    print(f"val match? {sol == ans}")
 
-test_sub_rsub()
+    x = ad.Variable(3, label="x")
+    y = ad.exp(x)
+    ans_val, ans_der = y.val, y.der["x"]
+    sol_val, sol_der = np.exp(3), np.exp(3)
+    print("ad.exp(var(3))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
 
-test_mul_rmul()
+    x = ad.Variable(3, label="x")
+    ans_val, ans_der = ad.exp(x).val, ad.exp(x).der["x"]
+    sol_val, sol_der = np.exp(3), np.exp(3)
+    print("ad.exp(var(3))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
 
-test_truediv_rtruediv()
+    x = ad.Variable(3, label="x") + 3
+    y = ad.exp(x)
+    ans_val, ans_der = y.val, y.der["x"]
+    sol_val, sol_der = np.exp(6), np.exp(6)
+    print("ad.exp(var(3)+3)")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+    x = ad.Variable(3, label="x") + ad.Variable(4, label="y")
+    y = ad.exp(x)
+    ans_val, ans_der = y.val, [y.der["x"], y.der["y"]]
+    sol_val, sol_der = np.exp(7), [np.exp(7), np.exp(7)]
+    print("ad.exp(var(3) + var(4))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der[0], sol_der[0], rel_tol=1e-9, abs_tol=0.0) & math.isclose(ans_der[1], sol_der[1], rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+
+def test_ln():
+    print("-" * 50, "Test ln")
+    x = 3
+    ans = ad.ln(x)
+    sol = adnp.log(x)
+    print("ad.ln(3)")
+    print(f"val match? {sol == ans}")
+
+    x = ad.Variable(3, label="x")
+    y = ad.ln(x)
+    ans_val, ans_der = y.val, y.der["x"]
+    sol_val, sol_der = adnp.log(3), grad(adnp.log)(3.0)
+    print("ad.ln(var(3))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+    x = ad.Variable(3, label="x") + 3
+    y = ad.ln(x)
+    ans_val, ans_der = y.val, y.der["x"]
+    sol_val, sol_der = adnp.log(6), grad(lambda x: adnp.log(x + 3.0))(3.0)
+    print("ad.ln(var(3)+3)")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+    x = ad.Variable(3, label="x") + ad.Variable(4, label="y")
+    y = ad.ln(x)
+    ans_val, ans_der = y.val, [y.der["x"], y.der["y"]]
+    sol_val, sol_der = adnp.log(7), [
+        grad(lambda x, y: adnp.log(x + y), 0)(3.0, 4.0),
+        grad(lambda x, y: adnp.log(x + y), 1)(3.0, 4.0),
+    ]
+    print("ad.ln(var(3) + var(4))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der[0], sol_der[0], rel_tol=1e-9, abs_tol=0.0) & math.isclose(ans_der[1], sol_der[1], rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+
+def test_logistic():
+    print("-" * 50, "Test logistic")
+
+    def logistic(x):
+        return 1 / (1 + adnp.exp(-x))
+
+    x = 3
+    ans = ad.logistic(x)
+    sol = logistic(x)
+    print("ad.logistic(3)")
+    print(f"val match? {sol == ans}")
+
+    x = ad.Variable(3, label="x")
+    y = ad.logistic(x)
+    ans_val, ans_der = y.val, y.der["x"]
+    sol_val, sol_der = logistic(3), grad(logistic)(3.0)
+    print("ad.logistic(var(3))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+    x = ad.Variable(3, label="x") + 3
+    y = ad.logistic(x)
+    ans_val, ans_der = y.val, y.der["x"]
+    sol_val, sol_der = logistic(6), grad(lambda x: logistic(x + 3.0))(3.0)
+    print("ad.logistic(var(3)+3)")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+    x = ad.Variable(3, label="x") + ad.Variable(4, label="y")
+    y = ad.logistic(x)
+    ans_val, ans_der = y.val, [y.der["x"], y.der["y"]]
+    sol_val, sol_der = logistic(7), [
+        grad(lambda x, y: logistic(x + y), 0)(3.0, 4.0),
+        grad(lambda x, y: logistic(x + y), 1)(3.0, 4.0),
+    ]
+    print("ad.logistic(var(3) + var(4))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der[0], sol_der[0], rel_tol=1e-9, abs_tol=0.0) & math.isclose(ans_der[1], sol_der[1], rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+
+def test_log10():
+    print("-" * 50, "Test log10")
+
+    def log10(x):
+        return adnp.log(x) / adnp.log(10)
+
+    x = 3
+    ans = ad.log10(x)
+    sol = log10(x)
+    print("ad.log10(3)")
+    print(f"val match? {sol == ans}")
+
+    x = ad.Variable(3, label="x")
+    y = ad.log10(x)
+    ans_val, ans_der = y.val, y.der["x"]
+    sol_val, sol_der = log10(3), grad(log10)(3.0)
+    print("ad.log10(var(3))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+    x = ad.Variable(3, label="x") + 3
+    y = ad.log10(x)
+    ans_val, ans_der = y.val, y.der["x"]
+    sol_val, sol_der = log10(6), grad(lambda x: log10(x + 3.0))(3.0)
+    print("ad.log10(var(3)+3)")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der, sol_der, rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+    x = ad.Variable(3, label="x") + ad.Variable(4, label="y")
+    y = ad.log10(x)
+    ans_val, ans_der = y.val, [y.der["x"], y.der["y"]]
+    sol_val, sol_der = log10(7), [
+        grad(lambda x, y: log10(x + y), 0)(3.0, 4.0),
+        grad(lambda x, y: log10(x + y), 1)(3.0, 4.0),
+    ]
+    print("ad.log10(var(3) + var(4))")
+    print(
+        f"val match? {ans_val == sol_val}; der match? {math.isclose(ans_der[0], sol_der[0], rel_tol=1e-9, abs_tol=0.0) & math.isclose(ans_der[1], sol_der[1], rel_tol=1e-9, abs_tol=0.0)}"
+    )
+
+
+if __name__ == "__main__":
+    test_add_radd()
+    test_sub_rsub()
+    test_mul_rmul()
+    test_truediv_rtruediv()
+    test_exp()
+    test_ln()
+    test_logistic()
+    test_log10()
