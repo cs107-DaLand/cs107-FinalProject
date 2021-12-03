08f68524c439fed61843ba97827a88a181cd1433
sehgal-neil <35875729+neil-sehgal@users.noreply.github.com>
2021-12-03 09:58:11 -0500

    Updated newton example

--- src/run_newton_example.py
+++ src/run_newton_example.py
@@ -1,3 +1,11 @@
+"""
+This example is just to show how modules need to be run now, as oppposed to just executing them like
+python3 newton_draft.py in their directory.
+"""
 from cs107_salad.Optimization.newton_draft import *
 
+f = '2*x**3  - 2*x - 5'
+newton(f, 2, 1.0e-8, 100)
+
+# or can do 
 main()
