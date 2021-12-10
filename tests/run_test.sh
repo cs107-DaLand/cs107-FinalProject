0365b57c2a8286a60118ffa4c71fea48fed91aff
sehgal-neil <35875729+neil-sehgal@users.noreply.github.com>
2021-12-09 22:11:18 -0500

    disabled warnings in test script

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -2,6 +2,6 @@
 export PYTHONPATH='../src/'
 set -e
 # pytest --cov=src ./
-coverage run -m pytest test_forward.py -v
+coverage run -m pytest test_forward.py -v --disable-pytest-warnings
 coverage report --fail-under=90
 coverage html
